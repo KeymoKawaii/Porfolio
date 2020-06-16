43b3706d3928a1ba2ae8b912f0c79dc0cdea9f75
Paolo <keymo.kawaii@gmail.com>
2020-06-16 11:47:42 +0200

    test form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ const About = () => (
       <SEO title="Contact" />
       <h3>Contactez-moi</h3>
       <hr />
-      <form name="send-place" method="POST" enctype="multipart/form-data"  data-netlify="true">
+      {/* <form name="send-place" method="POST" enctype="multipart/form-data"  data-netlify="true">
         <p>
           <label>Votre Nom: <input type="text" name="name" /></label>   
         </p>
@@ -21,6 +21,24 @@ const About = () => (
         <p>
           <button type="submit">Envoyer</button>
         </p>
+      </form> */}
+      - <form method="post" action="#">
++ <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
++   <input type="hidden" name="bot-field" />
++   <input type="hidden" name="form-name" value="contact" />
+<p>
+          <label>Votre Nom: <input type="text" name="name" /></label>   
+        </p>
+        <p>
+          <label>Votre Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Envoyer</button>
+        </p>
+      </form>
       </form>
     </section>
 )
