4255abf4dc70f4a7e76406a888b1d19dd0315f73
Paolo <keymo.kawaii@gmail.com>
2020-06-16 10:31:46 +0200

    form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,23 +5,23 @@ import SEO from "../components/seo"
 
 const About = () => (
     <section className="PartBlanc Contact">
-    <SEO title="Contact" />
-    <h3>Contactez-moi</h3>
-    <hr />
-    <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
-      <p>
-        <label>Votre Nom: <input type="text" name="name" /></label>   
-      </p>
-      <p>
-        <label>Votre Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Envoyer</button>
-      </p>
-  </form>
+      <SEO title="Contact" />
+      <h3>Contactez-moi</h3>
+      <hr />
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>Votre Nom: <input type="text" name="name" /></label>   
+        </p>
+        <p>
+          <label>Votre Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Envoyer</button>
+        </p>
+      </form>
     </section>
 )
 
